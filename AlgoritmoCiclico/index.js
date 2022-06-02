c8815e1338b445b4f4f41f7bbe5480b42573611f
Witch <tattookillerqueen@gmail.com>
2022-06-01 23:34:51 -0300

    first commit

---
+++ AlgoritmoCiclico/index.js
@@ -0,0 +1,9 @@
+const names = ["Lola", "Malvina", "Franco", "Juan", "Betto", "Nico", "Distila", "Akemi"];
+
+for(name of names ){
+    console.log('Hola ' $name);
+}
+
+while (name of names != Nico) {
+    console.log(No tienes acceso al sitio)
+}
